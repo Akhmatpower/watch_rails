46830249577d89a90c7dcfca1ac8989e6b9f422a
Kaply9l <kislov.igor5@icloud.com>
2022-03-03 15:36:59 +0700

    Add cart part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  use_doorkeeper
+
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  get 'products/index'
+  resources :product, only: %i[new edit show destroy create]
+
+  resources :category, only: [:show]
+
+  resource :cart, only: %i[destroy show] do
+    resources :items, only: %i[destroy create]
+  end
+
+  resources :search, only: [:index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :profile,  only: [:index]
+      resources :products, only: [:index]
+    end
+  end
+
+  root 'main#index'
 end
